6054a1905b231f5e5d82a0b5a2e3b58bc6c1da93
utk-spartan <utkarsh.spartan@gmail.com>
2015-10-29 04:55:19 +0530

    added insert and moved select to header

--- admin.php
+++ admin.php
@@ -1,30 +1,6 @@
 <center>
 <?php 
 	include 'header.php';
-	
-	function select($que,$con){
-		$q=$con->query($que);
-		$row=$q->num_rows;
-		$col=$q->field_count;
-
-		echo "<table>";
-		for($j=0;$j<$col;$j++){
-				$c=$q->fetch_field();
-				echo "<th>",$c->name,"</th>";
-			}
-		echo "<br>";
-		for($i=0;$i<$row;$i++){
-			$r=$q->fetch_row();
-			echo "<tr>";
-			for($j=0;$j<$col;$j++){
-				echo "<td>",$r[$j],"</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";
-	}
-
-	
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 	<select name="value">
@@ -65,18 +41,29 @@
 				for($j=0;$j<$col;$j++){
 					echo "<td>",$r[$j],"</td>";
 				}
-				echo "<td><input type=text name=new></td></tr>";
+				echo "<td><input type=text name=new[]></td></tr>";
 			}
 			echo "</table>";
 			echo "<br><input type=submit name=conf_ins value=INSERT>";
-			
-			if(isset($_POST['conf_ins'])){
-				//$con->query("insert into $val values();");			
-			}
+			$_SESSION['row']=$row;
+			$_SESSION['ins_in']=$val;
 			
 		}
 		else if(isset($_POST['update'])){
 			//select("select id from employee;",$con);
+		}
+		else if(isset($_POST['conf_ins'])){
+				//$con->query("insert into $val values();");
+			$row=$_SESSION['row'];
+			$val=$_SESSION['ins_in'];
+			$q="";
+			for($i=0;$i<$row;$i++){
+					$q=$q.$_POST['new'][$i];
+					if($i!=$row-1)$q=$q.",";
+				}
+			$q="insert into $val values($q);";
+			echo $q;
+			//$con->query($q);
 		}
 	?>
 	
--- header.php
+++ header.php
@@ -7,4 +7,28 @@
 	if ($conn->connect_error) {
     		die("MYSQL Database Connection failed: " . $conn->connect_error);
 	} 
+
+
+	function select($que,$con){
+		$q=$con->query($que);
+		$row=$q->num_rows;
+		$col=$q->field_count;
+
+		echo "<table>";
+		for($j=0;$j<$col;$j++){
+				$c=$q->fetch_field();
+				echo "<th>",$c->name,"</th>";
+			}
+		echo "<br>";
+		for($i=0;$i<$row;$i++){
+			$r=$q->fetch_row();
+			echo "<tr>";
+			for($j=0;$j<$col;$j++){
+				echo "<td>",$r[$j],"</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
 ?>
+
