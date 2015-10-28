15d3530fc51eab0a6b46c597401864c934160b72
utk-spartan <utkarsh.spartan@gmail.com>
2015-10-29 02:16:21 +0530

    aligned with DB

--- admin.php
+++ admin.php
@@ -24,21 +24,61 @@
 		echo "</table>";
 	}
 
-	select("select * from employee;",$con);
-	select("select id from employee;",$con);
-	echo "<br>done";
-
+	
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 	<select name="value">
-		<option value="employee">Employees</option>
-		<option value="employee">Equipment</option>
-		<option value="employee">Medicine</option>
-		<option value="employee">Patients</option>
-		<option value="employee">Rooms</option>
+		<option value="doctor">Doctors</option>
+		<option value="nurse">Nurses</option>
+		<option value="receptionist">Receptionists</option>
+		<option value="equipments">Equipment</option>
+		<option value="medicine">Medicine</option>
+		<option value="patient">Patients</option>
+		<option value="room">Rooms</option>
 	</select>
 	<input type="submit" name="view" value="View">
-	<input type="submit" name="view" value="Insert">
-	<input type="submit" name="view" value="Update">
+	<input type="submit" name="insert" value="Insert">
+	<input type="submit" name="update" value="Update">
+	
+	<?php
+		if(isset($_POST['view'])){
+			$val=$_POST['value'];
+			select("select * from $val;",$con);
+
+		}
+		else if(isset($_POST['insert'])){
+			$val=$_POST['value'];
+			$q=$con->query("desc $val;");
+			$row=$q->num_rows;
+			$col=$q->field_count;
+
+			echo "<table>";
+			for($j=0;$j<$col;$j++){
+					$c=$q->fetch_field();
+					echo "<th>",$c->name,"</th>";
+				}
+			echo "<th>Insert</th>";
+			echo "<br>";
+			for($i=0;$i<$row;$i++){
+				$r=$q->fetch_row();
+				echo "<tr>";
+				for($j=0;$j<$col;$j++){
+					echo "<td>",$r[$j],"</td>";
+				}
+				echo "<td><input type=text name=new></td></tr>";
+			}
+			echo "</table>";
+			echo "<br><input type=submit name=conf_ins value=INSERT>";
+			
+			if(isset($_POST['conf_ins'])){
+				//$con->query("insert into $val values();");			
+			}
+			
+		}
+		else if(isset($_POST['update'])){
+			//select("select id from employee;",$con);
+		}
+	?>
+	
 </form>
 </center>
